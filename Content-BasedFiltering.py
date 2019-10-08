3d01f4be1744a6ebef5fbb2a83404866731b8eca
JakinHuangWang <31380172+JakinHuangWang@users.noreply.github.com>
2019-10-07 22:04:14 -0700

    Delete Content-BasedFiltering.py

--- Content-BasedFiltering.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-movies_df = pd.read_csv('ml-latest/movies.csv')
-ratings_df = pd.read_csv('ml-latest/ratings.csv')
-
-print(movies_df.head())
-
-movies_df['year'] = movies_df['year'].str.extract('\d\d\d\d', expand=False)
